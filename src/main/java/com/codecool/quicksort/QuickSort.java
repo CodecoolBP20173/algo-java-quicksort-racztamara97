2c56191419c6a5ccc49a73075497bec2a6577503
racztamara97 <racztamara97@gmail.com>
2018-09-09 22:49:30 +0200

    change numbers implemented

--- src/main/java/com/codecool/quicksort/QuickSort.java
+++ src/main/java/com/codecool/quicksort/QuickSort.java
@@ -8,7 +8,14 @@ public class QuickSort {
      * Sorts the given List in place
      * @param toSort the List to sort. Throws an error if its null
      */
-    public void sort(List<Integer> toSort) {
+    private void sort(List<Integer> toSort) {
+
+        private void changeNumbers(int numberToChange, int newNumber) {
+            int temp = toSort.get(numberToChange);
+            toSort.set(numberToChange, toSort.get(newNumber));
+            toSort.set(newNumber, temp);
+        }
 
     }
 }
+
