4e665169bb0f86550c7d7bc76c98717e8a9c6161
racztamara97 <racztamara97@gmail.com>
2018-09-10 18:03:15 +0200

    main implemented

--- src/main/java/com/codecool/quicksort/Main.java
+++ src/main/java/com/codecool/quicksort/Main.java
@@ -1,9 +1,19 @@
 package com.codecool.quicksort;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         // Use the tests to check your code!
+        QuickSort sorter = new QuickSort();
+        List<Integer> testArray = Arrays.asList(100, 20, 45, 25, 56, 50, 79, 28, 76, 88, 53, 12, 50);
+        sorter.sort(testArray);
+        for (int i:testArray) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
     }
 
 }
